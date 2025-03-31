8fc83dc2198087b15a0f6420cf128baecdf7866d
monark21 <sigmatest218@gmail.com>
2025-04-01 00:15:59 +0200

    Update config.js

--- config.js
+++ config.js
@@ -19,7 +19,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "PUT YOUR SESSION ID HERE",
+SESSION_ID: process.env.SESSION_ID || "SIGMA-MD~B8hyXbiC#glDBgliWFxobUrU3Rn5440zSV_CDwBUIRyDA16IRf60",
 AUTO_READ_STATUS: process.env.AUTO_READ_STATUS || "true",
 MODE: process.env.MODE || "private",
 AUTO_VOICE: process.env.AUTO_VOICE || "false",
